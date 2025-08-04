1534917ab0c6dd45e9bb07442d8ea3ec02f1abbc
PlayzByJake <154666246+PlayzByJake@users.noreply.github.com>
2025-08-03 22:28:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const PASSWORD = "mySecret123"; // change this
 
 function setCookie(name, value, days) {
   const d = new Date();
-  d.setTime(d.getTime() + (days*24*60*60*1000));
-  const expires = "expires="+ d.toUTCString();
+  d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
+  const expires = "expires=" + d.toUTCString();
   document.cookie = name + "=" + value + ";" + expires + ";path=/";
 }
 
@@ -11,7 +11,7 @@ function getCookie(name) {
   const cname = name + "=";
   const decoded = decodeURIComponent(document.cookie);
   const ca = decoded.split(';');
-  for(let i = 0; i < ca.length; i++) {
+  for (let i = 0; i < ca.length; i++) {
     let c = ca[i].trim();
     if (c.indexOf(cname) === 0) return c.substring(cname.length, c.length);
   }
@@ -27,9 +27,14 @@ function checkAccess() {
 function handleLogin() {
   const input = document.getElementById("password").value;
   if (input === PASSWORD) {
-    setCookie("auth", "true", 1); // 1 day expiration
+    setCookie("auth", "true", 1); // 1 day
     window.location.href = "home.html";
   } else {
     alert("Wrong password!");
   }
+}
+
+// Automatically check access when this file loads, but only if we're NOT on the login page
+if (!window.location.pathname.endsWith("index.html")) {
+  checkAccess();
 }
